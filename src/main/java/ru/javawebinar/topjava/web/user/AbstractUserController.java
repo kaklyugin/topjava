c2e4ba58bf6613728e63e44bb416d9d487fa2d89
Anton <k_anton1@mail.ru>
2024-02-09 17:21:12 +0300

    Revert "Meal extends AbstractBaseEntity; MealTo moved to ru.javawebinar.topjava.to . Added test users."
    This reverts commit 400ab643

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
 
@@ -14,7 +13,7 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
-    
+
     @Autowired
     private UserService service;
 
